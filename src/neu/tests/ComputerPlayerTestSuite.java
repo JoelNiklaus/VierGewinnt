b428f0aa519c4d9ea59717e11ec14facdb6027b9
Dominik Briner <dominik.briner@students.unibe.ch>
2016-11-30 11:07:13 +0100

    Improved tests.

---
+++ src/neu/tests/ComputerPlayerTestSuite.java
@@ -0,0 +1,13 @@
+package neu.tests;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+/**
+ * Runs all tests that compare the ComputerPlayer other to levels
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ComputerPlayerTestLevel1.class, ComputerPlayerTestLevel2.class})
+public class ComputerPlayerTestSuite {
+
+
+}
